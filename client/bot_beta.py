1ae509217785a6b971e42119c61bc7c7259a8d74
mhbackes <mhbackes@inf.ufrgs.br>
2015-04-27 22:12:03 -0300

    Bot beta atualizado

--- client/bot_beta.py
+++ client/bot_beta.py
@@ -19,161 +19,169 @@ NEG_INF = -10000
 
 # ============================================================================
 class Board(object):
-    def __init__(self, state):
-        self.cells = [[None for j in xrange(8)] for i in xrange(8)]
-        self.my_pieces = []
-        self.enemy_pieces = []
-        self.my_team = 0
-        
-        PIECES = {
-            'r': Rook,
-            'p': Pawn,
-            'b': Bishop,
-            'q': Queen,
-            'n': Knight,
-        }
+	def __init__(self, state):
+		self.cells = [[None for j in xrange(8)] for i in xrange(8)]
+		self.my_pieces = []
+		self.enemy_pieces = []
+		self.my_team = 0
+		self.removed_pieces = []
+		
+		PIECES = {
+			'r': Rook,
+			'p': Pawn,
+			'b': Bishop,
+			'q': Queen,
+			'n': Knight,
+		}
 
-        self.my_team = state['who_moves']
-        c = state['board']
-        i = 0
+		self.my_team = state['who_moves']
+		c = state['board']
+		i = 0
 
-        for row in xrange(7, -1, -1):
-            for col in xrange(0, 8):
-                if c[i] != '.':
-                    cls = PIECES[c[i].lower()]
-                    team = BLACK if c[i].lower() == c[i] else WHITE
+		for row in xrange(7, -1, -1):
+			for col in xrange(0, 8):
+				if c[i] != '.':
+					cls = PIECES[c[i].lower()]
+					team = BLACK if c[i].lower() == c[i] else WHITE
 
-                    piece = cls(self, team, (row, col))
-                    self.cells[row][col] = piece
+					piece = cls(self, team, (row, col))
+					self.cells[row][col] = piece
 
-                    if team == self.my_team:
-                        self.my_pieces.append(piece)
-                    else:
-                        self.enemy_pieces.append(piece)
+					if team == self.my_team:
+						self.my_pieces.append(piece)
+					else:
+						self.enemy_pieces.append(piece)
 
-                i += 1
-        
-        self.string = self._calculate_string()
-        self.value = self.calculate_value()
+				i += 1
 
-    def __getitem__(self, pos):
-        if not 0 <= pos[0] <= 7 or not 0 <= pos[1] <= 7:
-            return None
+		self.string = self._calculate_string()
+		self.value = self.calculate_value()
 
-        return self.cells[pos[0]][pos[1]]
+	def __getitem__(self, pos):
+		if not 0 <= pos[0] <= 7 or not 0 <= pos[1] <= 7:
+			return None
 
-    def __setitem__(self, pos, value):
-        self._cells[pos[0]][pos[1]] = value
+		return self.cells[pos[0]][pos[1]]
 
-    def __lt__(self, other):
-        return self.value < other.value
-    
-    def _calculate_string(self):
-        string = ''
-        for i in xrange(8):
-            for j in xrange(8):
-                piece = self.cells[i][j]
-                if  piece == None:
-                    string += '.'
-                else:
-                    string += piece.to_string()
-        return string
+	def __setitem__(self, pos, value):
+		self._cells[pos[0]][pos[1]] = value
 
-    def is_empty(self, pos):
-        return self[pos] is None
-    
-    def generate(self):
-        moves = []
-        for piece in self.my_pieces:
-            ms = piece.generate()
-            ms = [(piece.position, m) for m in ms]
-            moves.extend(ms)
-        move_board = []
-        for move in moves:
-            board = copy.deepcopy(self)
-            board.move(move)
-            pair = (move, board)
-            move_board.append(pair)
-        return move_board
-    
-    def pawns(self, pieces):
-        pawns = []
-        for piece in pieces:
-            if isinstance(piece, Pawn):
-                pawns.append(piece)
-        return pawns
-    
-    # retorna  1 se ganhou o jogo
-    #         -1 se perdeu o jogo
-    #          0 se o jogo nao acabou
-    def is_end(self):
-        enemy_pawns = self.pawns(self.enemy_pieces)
-        if enemy_pawns == []:
-            return 1
-        my_pawns = self.pawns(self.my_pieces)
-        if my_pawns == []:
-            return -1
+	def __lt__(self, other):
+		return self.value < other.value
 
-        if self.my_team == WHITE:
-            my_last_row = 7
-            enemy_last_row = 0
-        else:
-            my_last_row = 0
-            enemy_last_row = 7
-            
-        for pawn in my_pawns:
-            if pawn.position[0] == my_last_row:
-                return 1
-            
-        for pawn in enemy_pawns:
-            if pawn.position[0] == enemy_last_row:
-                return -1
-        return 0
-    
-    
-    # move a peca selecionada, atualiza as pecas do oponente  se uma peca foi
-    # eliminada e inverte as pecas dos jogadores
-    def move(self, move):
-        (old, new) = move
-        toBeRemoved = self.cells[new[0]][new[1]]
-        if toBeRemoved != None:
-            self.enemy_pieces.remove(toBeRemoved)
-        self.cells[new[0]][new[1]] = self.cells[old[0]][old[1]]
-        self.cells[new[0]][new[1]].position = new
-        self.cells[old[0]][old[1]] = None
-        
-        temp = self.my_pieces
-        self.my_pieces = self.enemy_pieces
-        self.enemy_pieces = temp
-        self.string = self._calculate_string()
-        self.value = self.calculate_value()
-        
-    def calculate_value(self):
-            end = self.is_end()
-            if end == 1:
-                    return POS_INF
-            if end == 0:
-                    return NEG_INF
-            value = 0.0
-            for piece in self.my_pieces:
-                    value += piece.value()
-            for piece in self.enemy_pieces:
-                    value -= piece.value()
-                
-    # avaliacao heuristica do tabuleiro atual:
-    # Q*9 + N*3 + P*1 - q*9 - n*3 - p*1
-    def calculate_value(self):
-        end = self.is_end()
-        if end == 1:
-            return POS_INF
-        if end == -1:
-            return NEG_INF
-        value = 0.0
-        for piece in self.my_pieces:
-            value += piece.value()
-        for piece in self.enemy_pieces:
-            value -= piece.value()
-        return value
+	def _calculate_string(self):
+		string = ''
+		for i in xrange(8):
+			for j in xrange(8):
+				piece = self.cells[i][j]
+				if  piece == None:
+					string += '.'
+				else:
+					string += piece.to_string()
+		return string
+
+	def is_empty(self, pos):
+		return self[pos] is None
+
+	def generate(self):
+		moves = []
+		for piece in self.my_pieces:
+			ms = piece.generate()
+			ms = [(piece.position, m) for m in ms]
+			moves.extend(ms)
+		move_value = []
+		for move in moves:
+			self.move(move)
+			value = self.value
+			self.unmove(move)
+			pair = (move, value)
+			move_value.append(pair)
+		return move_value
+
+	def pawns(self, pieces):
+		pawns = []
+		for piece in pieces:
+			if isinstance(piece, Pawn):
+				pawns.append(piece)
+		return pawns
+
+	# retorna  1 se ganhou o jogo
+	#         -1 se perdeu o jogo
+	#          0 se o jogo nao acabou
+	def is_end(self):
+		enemy_pawns = self.pawns(self.enemy_pieces)
+		if enemy_pawns == []:
+			return 1
+		my_pawns = self.pawns(self.my_pieces)
+		if my_pawns == []:
+			return -1
+
+		if self.my_team == WHITE:
+			my_last_row = 7
+			enemy_last_row = 0
+		else:
+			my_last_row = 0
+			enemy_last_row = 7
+
+		for pawn in my_pawns:
+			if pawn.position[0] == my_last_row:
+				return 1
+
+		for pawn in enemy_pawns:
+			if pawn.position[0] == enemy_last_row:
+				return -1
+		return 0
+
+
+	# move a peca selecionada, atualiza as pecas do oponente  se uma peca foi
+	# eliminada e inverte as pecas dos jogadores
+	def move(self, move):
+		(old, new) = move
+		(new_row, new_col) = new
+		(old_row, old_col) = old
+		toBeRemoved = self.cells[new_row][new_col]
+		self.removed_pieces.append(toBeRemoved)
+		if toBeRemoved != None:
+			self.enemy_pieces.remove(toBeRemoved)
+		self.cells[new_row][new_col] = self.cells[old_row][old_col]
+		self.cells[new_row][new_col].position = new
+		self.cells[old_row][old_col] = None
+
+		self.my_pieces, self.enemy_pieces = self.enemy_pieces, self.my_pieces
+
+		self.string = self._calculate_string()
+		self.value = self.calculate_value()
+
+	def unmove(self, move):
+		(old, new) = move
+		(new_row, new_col) = new
+		(old_row, old_col) = old
+		self.cells[old_row][old_col] = self.cells[new_row][new_col]
+		self.cells[old_row][old_col].position = old
+		lastRemoved = self.removed_pieces.pop()
+		self.cells[new_row][new_col] = lastRemoved
+		if lastRemoved != None:
+			self.my_pieces.append(lastRemoved)
+
+		self.my_pieces, self.enemy_pieces = self.enemy_pieces, self.my_pieces
+
+		self.string = self._calculate_string()
+		self.value = self.calculate_value()
+
+	# avaliacao heuristica do tabuleiro atual:
+	# Q*9 + N*3 + P*1 - q*9 - n*3 - p*1
+	def calculate_value(self):
+		end = self.is_end()
+		if end == 1:
+			return POS_INF
+		if end == -1:
+			return NEG_INF
+		value = 0.0
+		for piece in self.my_pieces:
+			value += piece.value()
+		for piece in self.enemy_pieces:
+			value -= piece.value()
+		return value
 
 class Piece(object):
     def __init__(self):
@@ -470,151 +478,161 @@ class Knight(Piece):
         
 # BOT =========================================================================
 class Bot(LiacBot):
-    name = 'Bot'
+	name = 'Bot'
 
-    def __init__(self):
-        super(Bot, self).__init__()
-        self.transpositionTable = TranspositionTable()
-        self.last_move = None
-        
-    # Retorna board de maior value da lista
-    def __maxTake(self, moves):
-        (move, board) = moves.head()
-        maxi = (move, board, board.value)
-        
-        for (m, board) in moves:
-            val = board.value
-            if val > maxi[2]:
-                maxi = (m, board, val)
-        newMoves = moves.remove(maxi[0:2])
-        return (maxi, newMoves)        
-    
-    def __sortMoves(self, moves):
-        for _ in range(5):
-            (maxi, newMoves) = self.__maxTake(moves)
-        for (_, board) in moves:
-            val = board.value
-    
-    def __negaScout(self, board, depth, alpha, beta):
-        if board.value == POS_INF: # WIN
-            return (None, POS_INF)
-        if board.value == NEG_INF: # LOSE
-            return (None, NEG_INF)
-        if depth == 0:
-            return (None, board.value)
-        
-        # busca na tabela de transposicao
-        ttEntry = self.transpositionTable.lookUp(board.string, depth)
-        if ttEntry != None:
-            move, value = ttEntry
-            return move, value
-        
-        moves = board.generate()
-        if moves == []: # DRAW
-            return (None, 0)
-        sorted(moves, key=itemgetter(1), reverse=True)
-        
-        firstChild = True
-        for moveBoard in moves:
-            (move, newBoard) = moveBoard
-            if firstChild:
-                firstChild = False
-                bestValue = -(self.__negaScout(newBoard, depth - 1, -beta, -alpha)[1])
-                bestMove = move
-            else:
-                score = -(self.__alphaBeta(newBoard, depth - 1, -alpha -1, -alpha)[1])
-                if alpha < score and score < beta:
-                    score = -(self.__alphaBeta(newBoard, depth - 1, -beta, -alpha)[1])
-                if bestValue < score:
-                    bestValue = score
-                    bestMove = move
-            alpha = max(alpha, bestValue)
-            if alpha >= beta:
-                break
-                
-        # armazena na tabela de transposicao
-        self.transpositionTable.store(board.string, depth, bestMove, bestValue)
-        
-        return (bestMove, bestValue)
-        
-    # gera o proximo movimento a partir da poda alfa e beta
-    def __alphaBeta(self, board, depth, alpha, beta):
-        if board.value == POS_INF: # WIN
-            return (None, POS_INF)
-        if board.value == NEG_INF: # LOSE
-            return (None, NEG_INF)
-        if depth == 0:
-            return (None, board.value)
-        
-        # busca na tabela de transposicao
-        ttEntry = self.transpositionTable.lookUp(board.string, depth)
-        if ttEntry != None:
-            move, value = ttEntry
-            return move, value
-        
-        moves = board.generate()
-        if moves == []: # DRAW
-            return (None, 0)
-        
-        sorted(moves, key=itemgetter(1), reverse=True)
-        
-        bestValue = NEG_INF
-        bestMove = moves[0][1]
-        for moveBoard in moves:
-            (move, newBoard) = moveBoard
-            val = -(self.__negaScout(newBoard, depth - 1, -beta, -alpha)[1])
-            if bestValue < val:
-                bestValue = val
-                bestMove = move
-            alpha = max(alpha, val)
-            if alpha >= beta:
-                break
-        
-        # armazena na tabela de transposicao
-        self.transpositionTable.store(board.string, depth, bestMove, bestValue)
-        
-        return (bestMove, bestValue)
+	def __init__(self, depth):
+		super(Bot, self).__init__()
+		self.transpositionTable = TranspositionTable()
+		self._depth = depth
+		self.last_move = None
+		
+	# Retorna board de maior value da lista
+	def __maxTake(self, moves):
+		(move, board) = moves.head()
+		maxi = (move, board, board.value)
+		
+		for (m, board) in moves:
+			val = board.value
+			if val > maxi[2]:
+				maxi = (m, board, val)
+		newMoves = moves.remove(maxi[0:2])
+		return (maxi, newMoves)		
+	
+	def __sortMoves(self, moves):
+		for _ in range(5):
+			(maxi, newMoves) = self.__maxTake(moves)
+		for (_, board) in moves:
+			val = board.value
+	
+	def __negaScout(self, board, depth, alpha, beta):
+		if board.value == POS_INF: # WIN
+			return (None, POS_INF)
+		if board.value == NEG_INF: # LOSE
+			return (None, NEG_INF)
+		if depth == 0:
+			return (None, board.value)
+		
+		# busca na tabela de transposicao
+		ttEntry = self.transpositionTable.lookUp(board.string, depth)
+		if ttEntry != None:
+			move, value = ttEntry
+			return (move, value)
+		
+		moves = board.generate()
+		if moves == []: # DRAW
+			return (None, 0)
+		sorted(moves, key=itemgetter(1), reverse=True)
+		
+		firstChild = True
+		for moveValue in moves:
+			(move, _) = moveValue
+			board.move(move)
+			if firstChild:
+				firstChild = False
+				bestValue = -(self.__negaScout(board, depth - 1, -beta, -alpha)[1])
+				bestMove = move
+			else:
+				score = -(self.__negaScout(board, depth - 1, -alpha -1, -alpha)[1])
+				if alpha < score and score < beta:
+					score = -(self.__negaScout(board, depth - 1, -beta, -alpha)[1])
+				if bestValue < score:
+					bestValue = score
+					bestMove = move
+			board.unmove(move)
+			alpha = max(alpha, bestValue)
+			if alpha >= beta:
+				break
+				
+		# armazena na tabela de transposicao
+		self.transpositionTable.store(board.string, depth, bestMove, bestValue)
+		
+		return (bestMove, bestValue)
+		
+	# gera o proximo movimento a partir da poda alfa e beta
+	def __alphaBeta(self, board, depth, alpha, beta):
+		if board.value == POS_INF: # WIN
+			return (None, POS_INF)
+		if board.value == NEG_INF: # LOSE
+			return (None, NEG_INF)
+		if depth == 0:
+			return (None, board.value)
+		
+		# busca na tabela de transposicao
+		ttEntry = self.transpositionTable.lookUp(board.string, depth)
+		if ttEntry != None:
+			move, value = ttEntry
+			return move, value
+		
+		moves = board.generate()
+		if moves == []: # DRAW
+			return (None, 0)
+		moves = sorted(moves, key=itemgetter(1), reverse=True)
+		
+		bestValue = NEG_INF
+		(bestMove, _) = moves[0]
+		for moveValue in moves:
+			(move, _) = moveValue
+			
+			board.move(move)
+			val = -(self.__alphaBeta(board, depth - 1, -beta, -alpha)[1])
+			board.unmove(move)
+			
+			if bestValue < val:
+				bestValue = val
+				bestMove = move
+			alpha = max(alpha, val)
+			if alpha >= beta:
+				break
+		
+		# armazena na tabela de transposicao
+		self.transpositionTable.store(board.string, depth, bestMove, bestValue)
+		
+		return (bestMove, bestValue)
 
-    def on_move(self, state):
-        print 'Generating a move...\n',
-        board = Board(state)
+	def on_move(self, state):
+		print 'Generating a move...\n',
+		board = Board(state)
 
-        if state['bad_move']:
-            print state['board']
-            raw_input()
+		if state['bad_move']:
+			print state['board']
+			raw_input()
 
-        t0 = time.time()
-        move, value = self.__alphaBeta(board, 3, NEG_INF, POS_INF)
-        t = time.time()
-        print 'Time:', t - t0
+		t0 = time.time()
+		move, value = self.__negaScout(board, self._depth, NEG_INF, POS_INF)
+		t = time.time()
+		print 'Time:', t - t0
 
-        self.last_move = move
-        print move, ' value: ', value
-        self.send_move(move[0], move[1])
-        self.color = state["who_moves"]
+		self.last_move = move
+		print move, ' value: ', value
+		self.send_move(move[0], move[1])
+		self.color = state["who_moves"]
 
-    def on_game_over(self, state):
-        if state['draw']:
-            print 'Draw!'
-        elif state['winner'] == self.color:
-            print 'We won!'
-        else:
-            print 'We lost!'
-        # sys.exit()
+	def on_game_over(self, state):
+		if state['draw']:
+			print 'Draw!'
+		elif state['winner'] == self.color:
+			print 'We won!'
+		else:
+			print 'We lost!'
+		# sys.exit()
 # =============================================================================
 
 if __name__ == '__main__':
-    color = 0
-    port = 50100
+	color = 0
+	port = 50100
+	depth = 4
 
-    if len(sys.argv) > 1:
-        if sys.argv[1] == 'black':
-            color = 1
-            port = 50200
+	if len(sys.argv) > 1:
+		if sys.argv[1] == 'black':
+			color = 1
+			port = 50200
+			
+	if len(sys.argv) > 2:
+		depth = int(sys.argv[2])
 
-    bot = Bot()
-    bot.port = port
+	bot = Bot(depth)
+	bot.port = port
 
-    bot.start()
+	bot.start()
 
 
